43b79005ea038d14b9dec9fd4cef1e1121a97bc3
Aia Rupsom <aiale88@gmail.com>
2020-11-10 16:11:58 +0200

    connect graphql with express

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
-const express = require('express')
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
-const app = express()
+const app = express();
+
+app.use('/graphql', graphqlHTTP({}));
 
 const SERVER_PORT = process.env.PORT || 4000;
 
-app.listen(SERVER_PORT, () => console.log(`Server up and running on port ${SERVER_PORT}...`));
+app.listen(SERVER_PORT, () =>
+  console.log(`Server up and running on port ${SERVER_PORT}...`)
+);
