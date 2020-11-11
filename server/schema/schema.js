17280e8ff7cd21004f2e9850c251a6b318a3f3bf
Aia Rupsom <aiale88@gmail.com>
2020-11-11 18:10:33 +0200

    establish relation between author and a book

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,13 +10,13 @@ const {
 } = graphql;
 
 // dummy data
-var books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+const books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
 ];
 
-var authors = [
+const authors = [
   { name: 'Patrick Rothfuss', age: 44, id: '1' },
   { name: 'Brandon Sanderson', age: 42, id: '2' },
   { name: 'Terry Pratchett', age: 66, id: '3' },
@@ -28,6 +28,12 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return find(authors, { id: parent.authorId });
+      },
+    },
   }),
 });
 
